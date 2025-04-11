dac4d4ee54bf77347e61b16bc4cb27c6207fcef6
Charanya-A <141047196+Charanya-A@users.noreply.github.com>
2025-04-11 13:39:21 +0530

    feat: refactor App component to use MainContent for improved routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import Hero from './components/Hero';
 import About from './components/About';
 import Education from './components/Education';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import WorkExp from './components/WorkExp';
+import Footer from './components/Footer';
+import Attributions from './pages/Attributions'; 
+import ShoutOut from './pages/ShoutOut';
+import Affiliates from './pages/Affiliates';
+
+const MainContent = () => (
+  <>
+    <Hero />
+    <About />
+    <WorkExp />
+    <Education />
+    <Projects />
+    <Contact />
+    <Footer />
+  </>
+);
 
 const App: React.FC = () => {
   return (
     <div className="app">
       <div className="content-wrapper">
-        <Hero />
-        <About />
-        <WorkExp />
-        <Education />
-        <Projects />
-        <Contact />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/attributions" element={<Attributions />} />
+          <Route path="/shout-outs" element={<ShoutOut />} />
+          <Route path="/affiliates" element={<Affiliates />} />
+
+        </Routes>
       </div>
     </div>
   );
