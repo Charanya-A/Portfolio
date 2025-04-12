212abb42f67d43fe812dc1486a10f08b05ee1285
Charanya-A <141047196+Charanya-A@users.noreply.github.com>
2025-04-12 15:33:05 +0530

    fix: update Contact section for improved semantics and styling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { MdEmail } from 'react-icons/md';
-import { FaLinkedin} from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import "../styles/Contact.css";
 
 const Contact: React.FC = () => {
   return (
-    <section className="section">
-      <h1 className="section-title">Connect</h1>
-      <div className="section-content contact-text">
-        <p>
+    <section className="connect-section">
+      <h2 className="connect-title">Connect</h2>
+      <div className="connect-card">
+        <p className="connect-text">
           Feel free to reach out to me via&nbsp;
           <a
             href="mailto:charanya.1si21is015@gmail.com"
             className="email-inline-icon"
             aria-label="Email Charanya"
           >
-            <a href="https://www.linkedin.com/in/charanya-amarnath-26169a246" target="_blank" rel="noopener noreferrer"><FaLinkedin className="footer-icon"/></a>
-            <MdEmail />
+            <MdEmail className="footer-icon" />
+          </a>
+          or through my&nbsp;
+          <a
+            href="https://www.linkedin.com/in/charanya-amarnath-26169a246"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="email-inline-icon"
+          >
+            <FaLinkedin className="footer-icon" />
           </a>
         </p>
       </div>
